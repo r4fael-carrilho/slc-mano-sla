7bf50ec1c64a874e7a7f653690eb8d4c0634d773
puts, é o Rafa. <00001096652870sp@al.educacao.sp.gov.br>
2024-08-12 17:16:43 -0300

    colocando os arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(400, 400);
+   background('pink');
+  }
+  function draw() {
+  if(mouseIsPressed){
+   rect(mouseX,mouseY,40,70); 
+    
+  }
+    
+    
+    
+  fill('black')  
+   
+  }
